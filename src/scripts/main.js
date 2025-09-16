b924a83d12d8782a5781d8bfcdedae9631b89a2c
Gutoprogrammer <guterobas@gmail.com>
2025-09-15 22:15:32 -0300

    criando a contagem regressiva

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,33 @@
-console.log('Olá Mundo!')
+AOS.init();
+
+const dataDoEvento = new Date("Dec 31, 2025 19:00:00");
+
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function(){
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+
+    const diaEmMS = 1000 * 60 * 60 * 24;
+    const horaEmMS = 1000 * 60 * 60;
+    const minutoEmMS = 1000 * 60;
+    const segundosEmMS = 1000
+
+    const diasAteOEvento = Math.floor(distanciaAteOEvento / diaEmMS);
+    const horasAteOEvento = Math.floor((distanciaAteOEvento % diaEmMS) / horaEmMS);
+    const minutosAteOEvento = Math.floor((distanciaAteOEvento % horaEmMS) / minutoEmMS);
+    const segundosAteOEvento = Math.floor((distanciaAteOEvento % minutoEmMS) / segundosEmMS);
+
+    
+    document.getElementById('contador').innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`
+
+    if(distanciaAteOEvento < 0){
+        clearInterval(contaAsHoras);
+         document.getElementById('contador').innerHTML = 'Evento expirado!'
+    }
+
+},1000)
+
+
